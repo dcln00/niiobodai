f4fde2908fc83fb5a3c0d39fb0aa33ec80655750
Nii Aryeh <nii.aryh@gmail.com>
2025-05-17 11:10:45 +0000

    debugging

--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -18,16 +18,16 @@ section#hero(class="pt-40 pb-20 lg:pb-24 bg-brand-black")
 			h1(class="text-3xl lg:text-5xl text-white tracking-tight font-light" v-motion-slide-bottom :duration="500") Blog
 section#posts(class="py-20 bg-brand-dark")
 	.container
-		//- div(v-if="error" class="lg:w-1/2 mx-auto")
+		div(v-if="error" class="lg:w-1/2 mx-auto")
 			.error-snackbar(class="border border-red-500 rounded-md p-4")
 				div(class="flex space-x-4 items-center")
 					svgo-close(class="flex-shrink-0 text-2xl !pb-0 text-red-500")
 					p(class="capitalize text-white selection:bg-red-500") Error fetching posts
-		//- div(v-if="status === 'pending'" class="lg:w-1/2 mx-auto space-y-4 animate-pulse 1s")
+		div(v-if="status === 'pending'" class="lg:w-1/2 mx-auto space-y-4 animate-pulse 1s")
 			div(class="h-48 lg:h-96 bg-neutral-800")
 			div(class="h-7 bg-neutral-700")
 			div(class="h-7 w-1/2 bg-neutral-700")
-		div
+		div(v-else)
 			div(v-for="(post, idx) in posts" :key="idx" class="lg:w-1/2 mx-auto mb-16 last:mb-0")
 					NuxtLink(:to="post.path")
 						div(class="space-y-4")
